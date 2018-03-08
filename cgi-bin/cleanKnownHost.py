b92c8ca9d1ddff264ada8588155ad21e48622dec
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-08 15:06:54 +0800

    add content.

--- cgi-bin/cleanKnownHost.py
+++ cgi-bin/cleanKnownHost.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python
 #coding:utf-8
-import os,cgi,json
+import os,cgi,json,re
 
 data=cgi.FieldStorage()
 user=data.getvalue('user')
 host=data.getvalue('host')
-
+rootfile='/root/.ssh/known_hosts'
+alvinfile='/sophiroth/alvin/.ssh/known_hosts'
 
 success = {"success":"True","code":0}
 fail = {"success":"False","code":1}
 unknown = {"success":"False","code":2}
+
 if user == 'root':
-    if os.system("sed -i '/%s/d' /root/.ssh/known_hosts"%host) == 0:
-        respense=json.dumps(success)
-    else:
-        respense = json.dumps(fail)
+    f1=open(rootfile,'r')
+    f1Content=f1.read()
+    f1.close()
+    f2=open(rootfile,'w')
+    f2.write(re.sub(r'.*%s.*'%host,'',f1Content))
+    f2.close()
+    respense=json.dumps(success)
 elif user == 'alvin':
-    if os.system("sed -i '/%s/d' /sophiroth/alvin/.ssh/known_hosts"%host) == 0:
-        respense=json.dumps(success)
-    else:
-        respense = json.dumps(fail)
+    f1=open(alvinfile,'r')
+    f1Content=f1.read()
+    f1.close()
+    f2=open(alvinfile,'w')
+    f2.write(re.sub(r'.*%s.*'%host,'',f1Content))
+    f2.close()
+    respense=json.dumps(success)
+    respense=json.dumps(success)
 else:
     respense = json.dumps(unknown)
+
 print("Content-Type: application/json")
 print()
 print(respense)
