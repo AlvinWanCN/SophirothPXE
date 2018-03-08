b6d528955a7139a4f74db2d567e3ffbedf09c826
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-08 11:13:10 +0800

    add content.

--- cgi-bin/cleanKnownHost.py
+++ cgi-bin/cleanKnownHost.py
@@ -3,16 +3,25 @@
 import os,cgi,json
 
 data=cgi.FieldStorage()
-key=data.getvalue('key')
+user=data.getvalue('user')
+host=data.getvalue('host')
+
 
 success = {"success":"True","code":0}
 fail = {"success":"False","code":1}
-
-if os.system("sed -i '/%s/d'"%key) == 0:
-    respense=json.dumps(success)
+unknown = {"success":"False","code":2}
+if user == 'root':
+    if os.system("sed -i '/%s/d' /root/.ssh/known_hosts"%host) == 0:
+        respense=json.dumps(success)
+    else:
+        respense = json.dumps(fail)
+elif user == 'alvin':
+    if os.system("sed -i '/%s/d' /sophiroth/alvin/.ssh/known_hosts"%host) == 0:
+        respense=json.dumps(success)
+    else:
+        respense = json.dumps(fail)
 else:
-    respense = json.dumps(fail)
-
+    respense = json.dumps(unknown)
 print("Content-Type: application/json")
 print()
 print(respense)
