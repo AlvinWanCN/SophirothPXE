847f3ff666e1c736b4c79441d53ad5d0a1da722f
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-29 15:38:54 +0800

    update

--- cgi-bin/resetHostFingerprint.py
+++ cgi-bin/resetHostFingerprint.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python3
 #coding:utf-8
-import os,cgi,json,sys
+import os,cgi,json,re
 print("Content-Type: application/json")
 print('')
+
 data=cgi.FieldStorage()
 user=data.getvalue('user')
 host=data.getvalue('host')
-
+rootfile='/root/.ssh/known_hosts'
+alvinfile='/sophiroth/alvin/.ssh/known_hosts'
 
 success = {"success":"True","code":0}
 fail = {"success":"False","code":1}
 unknown = {"success":"False","code":2}
-if user == 'root':
-    if os.system("sudo sed -i '/%s/d' /root/.ssh/known_hosts"%host) == 0:
-        respense=json.dumps(success)
-    else:
+
+
+def processData(user,host):
+    global respense
+    if user == 'root':
+        file='/root/.ssh/known_hosts'
+    elif user == 'alvin':
+        file='/sophiroth/alvin/.ssh/known_hosts'
+
+    f1=open(file,'r')
+    f1Content=f1.read().split('\n')
+    f1.close()
+    f2=open(file,'w')
+    for i in f1Content:
+        b=re.sub(r'.*%s.*'%host,'',i)
+        if len(b) > 3:
+            f2.write(b+'\n')
+    f2.close()
+    respense = json.dumps(success)
+
+    if user not in ('alvin','root'):
         respense = json.dumps(fail)
-elif user == 'alvin':
-    if os.system("sed -i '/%s/d' /sophiroth/alvin/.ssh/known_hosts"%host) == 0:
-        respense=json.dumps(success)
-    else:
-        respense = json.dumps(fail)
-else:
-    respense = json.dumps(unknown)
 
-os.system('rpm -q expect &>/dev/null|| sudo yum install expect -y &>/dev/null')
-
-
-
-
-print(respense)
-
-a=os.system('''
+processData(user,host)
+os.system('''
 /usr/bin/expect <<eof
 spawn ssh %s
 expect {
@@ -42,3 +48,5 @@ expect eof
 eof
 '''%host)
 
+
+print(respense)
