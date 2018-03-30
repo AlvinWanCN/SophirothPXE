1dedfda2193ef9152022ee3607d97d9d708f754a
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-30 11:41:22 +0800

    update

--- cgi-bin/accep_salt-minion.py
+++ cgi-bin/accep_salt-minion.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 #coding:utf-8
-import subprocess
+import subprocess,cgi
 print("Content-type:text/html")
 print('')
+
+data=cgi.FieldStorage()
+password=data.getvalue('pw')
+
+if   password != 'sophiroth':
+    print('wrong authentication.')
+    exit(2)
 
 if str(subprocess.call('sudo salt-key -A  -y',shell=True)) == '0':
     print('success')
--- readme.md
+++ readme.md
@@ -4,3 +4,8 @@
 ## Directory introduction
 
 Sophiroth PXE System.
+
+启动服务的方式：
+cd到 当前目录后。
+
+nohup /usr/bin/python3 -m http.server --cgi 8001 & >/tmp/8001.log &
