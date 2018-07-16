8cfe42d273d7f1219c25a4efbb013037b114ca0b
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-07-16 15:29:04 +0800

    deleted some files and directory.

--- cgi-bin/set_dns.py
+++ cgi-bin/set_dns.py
@@ -4,23 +4,27 @@ import subprocess,cgi,hashlib,json
 print("Content-Type: application/json")
 print('')
 
-hl = hashlib.md5()
-data=cgi.FieldStorage()
-ip=data.getvalue('ip')
-hostname=data.getvalue('hostname')
-password=hl.update(data.getvalue('password').encode(encoding='utf-8'))
-dns_server='dns.alv.pub'
-file='/root/alv.pub.zone'
+
+try:
+    hl = hashlib.md5()
+    data=cgi.FieldStorage()
+    ip=data.getvalue('ip')
+    hostname=data.getvalue('hostname')
+    password=hl.update(data.getvalue('password').encode(encoding='utf-8'))
+    dns_server='dns.alv.pub'
+    file='/root/alv.pub.zone'
+except Exception as e:
+    print(json.dumps({'code':2,'message':e}))
 if  password == 'a006971e8a57f1cff1a44de29a9314f5':
     if ip and hostname:
         subprocess.call("""sudo salt 'dns.alv.pub' cmd.run 'sed -i "/%s/d" %s'"""%(hostname,file),shell=True)
         subprocess.call("""sudo salt 'dns.alv.pub' cmd.run 'echo %s A %s >> %s'"""%(hostname,ip,file),shell=True)
         subprocess.call("""sudo salt 'dns.alv.pub' cmd.run 'systemctl restart named'""",shell=True)
-        print(json.dump({'code': 0, 'message': 'success'}))
+        print(json.dumps({'code': 0, 'message': 'success'}))
     else:
-        print(json.dump({'code': 1, 'message': 'ip and hostname cannot null.'}))
+        print(json.dumps({'code': 1, 'message': 'ip and hostname cannot null.'}))
 else:
-    print(json.dump({'code':2,'message':'Password error'}))
+    print(json.dumps({'code':2,'message':'Password error'}))
 
 
 
