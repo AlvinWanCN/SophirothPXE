f545d46561055dd80b80078d262ae77f72bc4e17
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-07-16 15:09:05 +0800

    deleted some files and directory.

--- cgi-bin/set_dns.py
+++ cgi-bin/set_dns.py
@@ -10,11 +10,11 @@ ip=data.getvalue('ip')
 hostname=data.getvalue('hostname')
 password=hl.update(data.getvalue('password').encode(encoding='utf-8'))
 dns_server='dns.alv.pub'
-
+file='/root/alv.pub.zone'
 if  password == 'a006971e8a57f1cff1a44de29a9314f5':
     if ip and hostname:
-        subprocess.call("""sudo salt 'dns.alv.pub' cmd.run 'sed "/diana/d" /root/alv.pub.zone'""",shell=True)
-        subprocess.call("""sudo salt 'dns.alv.pub' cmd.run 'echo %s A %s >> /root/alv.pub.zone'""",shell=True)
+        subprocess.call("""sudo salt 'dns.alv.pub' cmd.run 'sed -i "/%s/d" %s'"""%(hostname,file),shell=True)
+        subprocess.call("""sudo salt 'dns.alv.pub' cmd.run 'echo %s A %s >> %s'"""%(hostname,ip,file),shell=True)
         subprocess.call("""sudo salt 'dns.alv.pub' cmd.run 'systemctl restart named'""",shell=True)
         print(json.dump({'code': 0, 'message': 'success'}))
     else:
